8bb84abfb6143e943db9b49dd6748a5d2aecb5e5
ucchi0713 <69422695+ucchi0713@users.noreply.github.com>
2025-11-12 15:27:24 +0900

    export_feed update

---
+++ create_idx_signals.sql
@@ -0,0 +1,3 @@
+create index if not exists idx_signals_code_date on signals(code, date);
+create index if not exists idx_prices_code_date on prices(code, date);
+create index if not exists idx_runs_created_at on backtests_runs(created_at desc);
